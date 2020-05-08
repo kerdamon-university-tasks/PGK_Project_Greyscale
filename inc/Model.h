a7a69eeafb978da3a52f9b34c37eb8dd1b3196a6
Adrian Furman <fadikk367@gmail.com>
2020-05-08 20:37:32 +0200

    Little fixes :)

--- inc/Model.h
+++ inc/Model.h
@@ -17,18 +17,23 @@ namespace GrayscaleConverter
 		Model();
 		/// image thumbnail getter
 		const wxImage& GetImageThumbnail() const;
+		bool GetIsResultSaved() const;
+		bool GetIsConfigSaved() const;
 
 		/// member fields setters
 		void SetColorTolerance(double tolerance) { m_colorTolerance = tolerance; }
 		void SetRedChannel(int value) { m_redChannel = value;}
 		void SetBlueChannel(int value) { m_blueChannel = value; }
 		void SetGreenChannel(int value) { m_greenChannel = value; }
-		void SetKeptHue(float value) { m_keptHue = wxColor(128, 128, 128); };
+		void SetKeptHue(float value) { m_keptHue = wxColor{ 128, 128, 128 }; };
 		void SetWorkMode(WorkMode mode) { m_mode = mode; }
 		void SetIsKeptHue(bool flag) { m_isKeptHue = flag; }
+		void SetBichromeColour(const wxColour colour) { m_bichromeColour = colour; }
 		
 		void LoadImageFromFile(const wxString& filePath);
+		void LoadConfigFromFile(const wxString& filePath);
 		void SaveImageToFile(const wxString& filename);
+		void SaveConfigToFile(const wxString& filePath) const;
 		void ApplyParametersToImage();
 
 		/// EASTERKURWA EGG
@@ -39,16 +44,17 @@ namespace GrayscaleConverter
 		void AdjustImageThumbnail();
 		
 		wxImage* m_originalImage;
-		wxImage m_originalImageCopy;
-		wxImage m_imageThumbnail;
-		wxImage m_imageThumbnailCopy;
+		wxImage* m_originalImageCopy;
+		wxImage* m_imageThumbnail;
+		wxImage* m_imageThumbnailCopy;
 
 		wxColour m_keptHue;
+		wxColour m_bichromeColour;
 		WorkMode m_mode{ WorkMode::NONE };
 
 		double m_colorTolerance{ 0 };
 		
-		const int m_maxImageThumbnailSize{ 500 };
+		//const int m_maxImageThumbnailSize{ 500 };
 		
 		int m_redChannel{ 0 };
 		int m_blueChannel{ 0 };
