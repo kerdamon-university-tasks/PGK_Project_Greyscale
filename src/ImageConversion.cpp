340c56a68e1279bcd6e6cc1d278344c6c18bb78a
Fillipek <mar.lim.420@onet.pl>
2020-05-15 21:29:22 +0100

    fix oversaturation

--- src/ImageConversion.cpp
+++ src/ImageConversion.cpp
@@ -1,4 +1,6 @@
 #include "inc/ImageConversion.h"
+#include <wx/log.h>
+#include <wx/translation.h>
 
 namespace GreyscaleConverter
 {
@@ -10,7 +12,19 @@ namespace GreyscaleConverter
 
     void ImageConversion::ConvertToGreyScale(wxImage& orgImage, wxImage& copyImage, int chRed, int chGreen, int chBlue)
     {
-        copyImage = orgImage.ConvertToGreyscale(chRed / 200., chGreen / 200., chBlue / 200.);
+        double redChannel = chRed/400. + 0.5;
+        double greenChannel = chGreen/400. + 0.5;
+        double blueChannel = chBlue/400. + 0.5;
+
+    	if(redChannel + greenChannel + blueChannel > 1)
+    	{
+            double adjustment{ (redChannel + greenChannel + blueChannel) };
+            redChannel /= adjustment;
+            greenChannel /= adjustment;
+            blueChannel /= adjustment;
+    	}
+
+        copyImage = orgImage.ConvertToGreyscale(redChannel, greenChannel, blueChannel);
     }
 
 }
